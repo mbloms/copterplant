9b3d7e8c129a677a6c7a305b5c48a950f9892cbf
David Skeppstedt <david.skeppstedt@gmail.com>
2015-05-04 14:41:17 +0200

    Added the project

---
+++ desktop/src/se/mad/copterplant/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.mad.copterplant.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.mad.copterplant.Copterplant;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Copterplant(), config);
+	}
+}
---
+++ html/src/se/mad/copterplant/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package se.mad.copterplant.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import se.mad.copterplant.Copterplant;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Copterplant();
+        }
+}
