83816cb70fa4a4982a19dda7ef4f0cfdd74c4cdb
David Skeppstedt <david.skeppstedt@gmail.com>
2015-05-06 15:43:38 +0200

    Tweak: Set up the first game screen. This will render a red background in full screen. You can exit the program with the ESC key.

--- core/src/se/mad/copterplant/screens/GameScreen.java
+++ core/src/se/mad/copterplant/screens/GameScreen.java
@@ -1,9 +1,10 @@
 package se.mad.copterplant.screens;
 
+import se.mad.copterplant.Copterplant;
 import se.mad.copterplant.util.GLUtil;
-
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 
 public class GameScreen extends SimpleScreen {
@@ -24,8 +25,10 @@ public class GameScreen extends SimpleScreen {
 
 	@Override
 	public void draw() {
+		Copterplant.CAMERA.update();
 		GLUtil.CLEAR_Window(Color.RED);
-		
+		Copterplant.RENDERER.setProjectionMatrix(Copterplant.CAMERA.combined);
+		//Here we can render stuff.
 	}
 
 }
