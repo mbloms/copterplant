a296a9be86c2023d2de458aacc764af86bf35ab6
Andreas Brommund <brommund@kth.se>
2015-05-06 16:18:20 +0200

    Tweak: Chanced the input parameters from float x and float y to Vector2 and added comments.

--- core/src/se/mad/copterplant/actor/Shape.java
+++ core/src/se/mad/copterplant/actor/Shape.java
@@ -1,7 +1,14 @@
 package se.mad.copterplant.actor;
 
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-
+import com.badlogic.gdx.math.Vector2;
+/**
+ * 
+ * Handle and print out the shapes.
+ * 
+ * @author Andreas Brommund
+ *
+ */
 public class Shape {
 	
 	private enum Type{
@@ -12,36 +19,36 @@ public class Shape {
  	private float width, height, radius;
  	private float[] vertices;
  	
-	public Shape(float width, float height) {
+	protected Shape(float width, float height) {
 		type = Type.Rectangle;
 		this.width = width;
 		this.height = height;
 	}
 	
-	public Shape(float radius){
+	protected Shape(float radius){
 		type = Type.Circle; 
 		this.radius = radius;
 	}
 	
-	public Shape(float[] vertices){
+	protected Shape(float[] vertices){
 		type = Type.Polygon;
 		this.vertices = vertices; 
 	}
 	
-	protected void renderShape(ShapeRenderer  renderer, float x, float y){
+	protected void renderShape(ShapeRenderer  renderer, Vector2 pos){
 		switch (type) {
 		case Circle:
-			renderer.circle(x, y, radius);
+			renderer.circle(pos.x,pos.y, radius);
 			break;
 		case Rectangle:
-			renderer.rect(x, y, width, height);
+			renderer.rect(pos.x, pos.y, width, height);
 			break;
 		case Polygon:
 			for(int i = 0;i < vertices.length;i++){
-				if(i%2 == 0){
-					vertices[i] *= x;
+				if(i%2 == 0){ //TODO it may be the opposite.  
+					vertices[i] *= pos.x;
 				}else{
-					vertices[i] *= y;
+					vertices[i] *= pos.y;
 				}
 			}
 			renderer.polygon(vertices);
