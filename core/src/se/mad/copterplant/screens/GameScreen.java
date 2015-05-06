74b72b8e0f4be5062546aecb54380d71ab21ce08
David Skeppstedt <david.skeppstedt@gmail.com>
2015-05-06 15:27:37 +0200

    Add: First implementation of the game screen.

---
+++ core/src/se/mad/copterplant/screens/GameScreen.java
@@ -0,0 +1,31 @@
+package se.mad.copterplant.screens;
+
+import se.mad.copterplant.util.GLUtil;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Color;
+
+
+public class GameScreen extends SimpleScreen {
+
+	public GameScreen(Game game) {
+		super(game);
+	}
+
+	@Override
+	public void init() {
+		
+	}
+
+	@Override
+	public void update(float delta) {
+		
+	}
+
+	@Override
+	public void draw() {
+		GLUtil.CLEAR_Window(Color.RED);
+		
+	}
+
+}
