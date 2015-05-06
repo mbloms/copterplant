23050e805e683554956b8346400217b23e90a7f0
David Skeppstedt <david.skeppstedt@gmail.com>
2015-05-06 14:04:41 +0200

    Tweak: Added some informative documentation.

--- core/src/se/mad/copterplant/util/Settings.java
+++ core/src/se/mad/copterplant/util/Settings.java
@@ -1,5 +1,11 @@
 package se.mad.copterplant.util;
-
+/**
+ * Settings
+ * A class to hold game wide settings.
+ * E.g: The target resolution for the game.
+ * @author DavidSkeppstedt
+ *
+ */
 public class Settings {
 		public static int GAME_WIDTH = 1280;
 		public static int GAME_HEIGHT = 800;
