c2bb6c3649f4c5b5d82719947cd1d8be74fa1f81
David Skeppstedt <david.skeppstedt@gmail.com>
2015-05-06 14:48:14 +0200

    Tweak: Set up some more parameters in the settings file and also made the game use these parameters.

--- core/src/se/mad/copterplant/util/Settings.java
+++ core/src/se/mad/copterplant/util/Settings.java
@@ -7,6 +7,7 @@ package se.mad.copterplant.util;
  *
  */
 public class Settings {
-		public static int GAME_WIDTH = 1280;
-		public static int GAME_HEIGHT = 800;
+	public static final String GAME_NAME = "CopterPlant";
+	public static final int GAME_WIDTH = 1280;
+	public static final int GAME_HEIGHT = 800;
 }
--- desktop/src/se/mad/copterplant/desktop/DesktopLauncher.java
+++ desktop/src/se/mad/copterplant/desktop/DesktopLauncher.java
@@ -2,11 +2,16 @@ package se.mad.copterplant.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import se.mad.copterplant.Copterplant;
+import se.mad.copterplant.util.Settings;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = Settings.GAME_NAME;
+		config.width = Settings.GAME_WIDTH;
+		config.height =Settings.GAME_HEIGHT;
 		new LwjglApplication(new Copterplant(), config);
 	}
 }
