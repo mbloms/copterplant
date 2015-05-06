ea804b7c96623e61fbc4f06f79465a294285e9d7
Andreas Brommund <brommund@kth.se>
2015-05-06 16:13:26 +0200

    Tweak: Added comments. And added a updateCollisionbox and call the method in setPos

--- core/src/se/mad/copterplant/actor/Actor.java
+++ core/src/se/mad/copterplant/actor/Actor.java
@@ -3,44 +3,74 @@ package se.mad.copterplant.actor;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-
+/**
+ * 
+ * The actor class handle the position velocity and the collision box. 
+ * You can also specify which shape the actor should have. 
+ * @author Andreas Brommund
+ *
+ */
 
 public abstract class Actor extends Shape{
 	private Vector2 pos;
 	private Vector2 vel;
 	private Rectangle collisionBox;
 	
-	public Actor(Vector2 pos, Vector2 vel, float width, float height) {
+	/**
+	 * Create a rectangle shape to the actor, you need to specify a velocity.
+	 * 
+	 * @param pos
+	 * @param vel
+	 * @param width
+	 * @param height
+	 */
+	protected Actor(Vector2 pos, Vector2 vel, float width, float height) {
 		super(width, height);
 		setPos(pos);
-		setPos(vel);
 		new Rectangle(vel.x, vel.y, width, height);
 	}
-	public Actor(Vector2 pos, Vector2 vel, float radius){
+	
+	/**
+	 * Create a circle shape to the actor, you need to specify a velocity.
+	 * 
+	 * @param pos
+	 * @param vel
+	 * @param radius
+	 */
+	protected Actor(Vector2 pos, Vector2 vel, float radius){
 		super(radius);
 		setPos(pos);
-		setPos(vel);
 		new Rectangle(vel.x, vel.y, radius*2, radius*2);
 	}
 	
-	public Actor(Vector2 pos, Vector2 vel, float[] vertices){
+	
+	/**
+	 * Create a polygon shape to the actor, you need to specify a velocity.
+	 * 
+	 * @param pos
+	 * @param vel
+	 * @param vertices
+	 */
+	protected Actor(Vector2 pos, Vector2 vel, float[] vertices){
 		super(vertices);
 		setPos(pos);
-		setPos(vel);
 		new Rectangle(vel.x, vel.y, 100, 100); //TODO Calculate width
 	}
-	
-	public abstract void init();
-	public abstract void update(float delta);
-	public abstract void draw(ShapeRenderer  renderer);
+
+	private void updateCollisionbox(){
+		collisionBox.x = pos.x;
+		collisionBox.y = pos.y;
+	}
 
 	public Vector2 getPos() {
+		
 		return pos;
 	}
 
 
 	public void setPos(Vector2 pos) {
 		this.pos = pos;
+		updateCollisionbox();
 	}
 
 
@@ -52,13 +82,16 @@ public abstract class Actor extends Shape{
 	public void setVel(Vector2 vel) {
 		this.vel = vel;
 	}
-	
+	/**
+	 * Return the collision box, you use to detect collision.
+	 *
+	 * @return collision box
+	 */
 	public Rectangle getCollisionBox() {
 		return collisionBox;
 	}
 	
-	public void updateCollisionbox(){
-		collisionBox.x = pos.x;
-		collisionBox.y = pos.y;
-	}
+	public abstract void init();
+	public abstract void update(float delta);
+	public abstract void draw(ShapeRenderer  renderer);
 }
