b8f25b9ec02c8ca41385477485b69e9dc99b7def
David Skeppstedt <david.skeppstedt@gmail.com>
2015-05-06 15:42:35 +0200

    Tweak: Enabled fullscreen

--- desktop/src/se/mad/copterplant/desktop/DesktopLauncher.java
+++ desktop/src/se/mad/copterplant/desktop/DesktopLauncher.java
@@ -12,6 +12,7 @@ public class DesktopLauncher {
 		config.title = Settings.GAME_NAME;
 		config.width = Settings.GAME_WIDTH;
 		config.height =Settings.GAME_HEIGHT;
+		config.fullscreen = true;
 		new LwjglApplication(new Copterplant(), config);
 	}
 }
