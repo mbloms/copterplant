fae1fd896268826f19544c4c9a4cc4fb97d6c96b
David Skeppstedt <david.skeppstedt@gmail.com>
2015-05-08 13:35:59 +0200

    Tweak: Removed example code for collidable.

--- core/src/se/mad/copterplant/screens/GameScreen.java
+++ core/src/se/mad/copterplant/screens/GameScreen.java
@@ -1,18 +1,14 @@
 package se.mad.copterplant.screens;
 import java.util.ArrayList;
-
 import se.mad.copterplant.Copterplant;
-import se.mad.copterplant.actor.Box;
 import se.mad.copterplant.actor.Collidable;
 import se.mad.copterplant.util.GLUtil;
 import se.mad.copterplant.util.UserInput;
-
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Color;
 
 
 public class GameScreen extends SimpleScreen {
-	private Box superbox;
 	private ArrayList<Collidable> actors;
 	public GameScreen(Game game) {
 		super(game);
@@ -20,28 +16,12 @@ public class GameScreen extends SimpleScreen {
 
 	@Override
 	public void init() {
-		actors = new ArrayList<Collidable>();
-		superbox = new Box();
-		
-		for (int i = 0; i<10; i++){
-			Box b = new Box();
-			actors.add(b);
-		}
-		
-		
 		
 	}
 
 	@Override
 	public void update(float delta) {
 		UserInput.POLL_USER_INPUT();
-		
-		for(Collidable c:actors){
-			if (superbox.isColliding(c)){
-				superbox.collide(c);
-				c.collide(superbox);
-			}
-		}
 	}
 
 	@Override
