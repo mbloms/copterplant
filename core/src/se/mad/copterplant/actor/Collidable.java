40c46589f99e69728cd49b7d3b989d94628d5838
David Skeppstedt <david.skeppstedt@gmail.com>
2015-05-09 21:18:04 +0200

    Tweak: Updated the interface Collidable to have Actor parameters instead.

--- core/src/se/mad/copterplant/actor/Collidable.java
+++ core/src/se/mad/copterplant/actor/Collidable.java
@@ -5,7 +5,7 @@ public interface Collidable {
 	 * This method could be executed when two collidable actors collide.
 	 * @param Collidable other The collidable actor that is colliding with this one.
 	 */
-	public void collide(Collidable other);
+	public void collide(Actor other);
 	/**
 	 * Checks whatever two collidable actors are colliding and how.
 	 * @param other
