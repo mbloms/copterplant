4ec4f547c1a7414c9191fb6f0f09a1d35bf4ad57
Andreas Brommund <brommund@kth.se>
2015-05-12 22:04:32 +0200

    Tweak: Collision boxes blue boxes

--- core/src/se/mad/copterplant/actor/Path.java
+++ core/src/se/mad/copterplant/actor/Path.java
@@ -17,11 +17,13 @@ import com.badlogic.gdx.math.Vector2;
 public class Path{
 	private LinkedList<Vector2> path; 
 	private LinkedList<Rectangle> collisionBox;
+	private LinkedList<Rectangle> collisionBoxTemp;
 	float radius = 10;
 	
 	public Path(Vector2 node){
 		path = new LinkedList<Vector2>();
 		collisionBox =  new LinkedList<Rectangle>();
+		collisionBoxTemp = new LinkedList<Rectangle>();
 		path.addLast(node);
 	}
 	
@@ -31,10 +33,18 @@ public class Path{
 	 */
 	public void addNode(Vector2 node){
 		path.addLast(node);
+		updateCollision();
 	}
 	
 	public LinkedList<Vector2> getPath(){
 		return path;
+	}
+	
+	private void updateCollision(){
+		
+		collisionBox.addAll(collisionBoxTemp);
+		
+		collisionBoxTemp.clear();
 	}
 	
 	/**
@@ -44,6 +54,8 @@ public class Path{
 	 * @param playerVel
 	 */
 	public void draw(ShapeRenderer renderer,Vector2 playerPos,Vector2 playerVel) {
+		collisionBoxTemp.clear();
+		
 		renderer.begin(ShapeType.Filled);
 		renderer.setColor(Color.BLUE);
 		
@@ -59,7 +71,6 @@ public class Path{
 					float y = old.y;
 					for(int i = 0; i < times;i++){
 						renderer.circle(x, y, radius);
-						collisionBox.addLast(new Rectangle(x-radius, y-radius, radius*2, radius*2)); //Should not do this here, but I don't want to loop one more time.
 						y += Math.signum(c.y-old.y)*radius*2;
 					}
 				}else{//Moving in the X direction
@@ -68,7 +79,6 @@ public class Path{
 					float x = old.x;
 					for(int i = 0; i < times;i++){
 						renderer.circle(x, y, radius);
-						collisionBox.addLast(new Rectangle(x-radius, y-radius, radius*2, radius*2)); //Should not do this here, but I don't want to loop one more time.
 						x += Math.signum(c.x-old.x)*radius*2;
 					}
 				}
@@ -83,7 +93,7 @@ public class Path{
 			float y = old.y;
 			for(int i = 0; i < times;i++){
 				renderer.circle(x, y, radius);
-				collisionBox.addLast(new Rectangle(x-radius, y-radius, radius*2, radius*2)); //Should not do this here, but I don't want to loop one more time.
+				collisionBoxTemp.addLast(new Rectangle(x-radius, y-radius, radius*2, radius*2)); //Should not do this here, but I don't want to loop one more time.
 				y += Math.signum(playerVel.y)*radius*2;
 			}
 		}else{//Moving in the X direction
@@ -92,7 +102,7 @@ public class Path{
 			float x = old.x;
 			for(int i = 0; i < times;i++){
 				renderer.circle(x, y, radius);
-				collisionBox.addLast(new Rectangle(x-radius, y-radius, radius*2, radius*2)); //Should not do this here, but I don't want to loop one more time.
+				collisionBoxTemp.addLast(new Rectangle(x-radius, y-radius, radius*2, radius*2)); //Should not do this here, but I don't want to loop one more time.
 				x += Math.signum(playerVel.x)*radius*2;
 			}
 		}
@@ -104,7 +114,12 @@ public class Path{
 		for(Rectangle r:collisionBox){
 			renderer.rect(r.x, r.y, r.width, r.height);
 		}
+		for(Rectangle r:collisionBoxTemp){
+			renderer.rect(r.x, r.y, r.width, r.height);
+		}
+	
 		renderer.end();
+		
 	}
 	
 	@Override
