c5c636c7ec7a2ee4e2b5a6dcf79f58075a3b729f
Andreas Brommund <brommund@kth.se>
2015-05-12 16:24:45 +0200

    Tweak: Let the path handle the path.

--- core/src/se/mad/copterplant/actor/Player.java
+++ core/src/se/mad/copterplant/actor/Player.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 /**
@@ -22,16 +23,15 @@ public class Player extends Actor implements Collidable{
 		
 	private boolean visit = true; //TODO Get from field
 	private boolean creatingPath;
-	private Vector2 prevNodeVel; 
+	private Vector2 prevNodeVel;
 	
 	private float speed = 4;
 	
-	LinkedList<Vector2> path; 
+	private Path path;
+	
 	
 	public Player(Vector2 pos) {
-
 		super(pos);
-		
 	}
 
 	@Override
@@ -39,8 +39,7 @@ public class Player extends Actor implements Collidable{
 		setShape(50f);
 		setShapeType(ShapeType.Filled);
 		setColor(Color.RED);
-		
-		path = new LinkedList<Vector2>();
+	
 		creatingPath = false;
 	}
 
@@ -62,18 +61,18 @@ public class Player extends Actor implements Collidable{
 			setVel(new Vector2(0, 0));
 			if(creatingPath){
 				creatingPath= false;
-				path.addLast(getPos());
-				if(path.getFirst().equals(path.get(1))){
+				path.addNode(getPos());
+				/*if(path.getFirst().equals(path.get(1))){
 					path.removeFirst();
-				}
+				}*/
 				System.out.println(path); // TODO Send to filed
-				path.clear();
+				path = null;
 			}
 		}else{
 			if(!creatingPath){
 				creatingPath = true; 
 				prevNodeVel = getVel();
-				path.addLast(getPos());
+				path = new Path(getPos());
 			}
 		}
 		
@@ -92,38 +91,27 @@ public class Player extends Actor implements Collidable{
 		
 		if(creatingPath&&(prevNodeVel.x != getVel().x || prevNodeVel.y != getVel().y)){
 			prevNodeVel = getVel();
-			path.addLast(getPos());
+			path.addNode(getPos());
 		}
 		setPos(getPos().add(getVel()));
 	}
 
 	@Override
 	public void draw(ShapeRenderer renderer) {
+		
+		if(creatingPath){
+			path.draw(renderer, getPos(), getVel());
+		}
+		
 		drawActor(renderer);
 		
 		renderer.begin(ShapeType.Line);
 		renderer.setColor(Color.GREEN);
 		renderer.rect(getCollisionBox().x, getCollisionBox().y,getCollisionBox().width, getCollisionBox().height);
 		
-		
-		
-		if(creatingPath){
-			Iterator<Vector2> it = path.iterator();
-			Vector2 old = null;
-			while (it.hasNext()) {
-				Vector2 c = it.next();
-				if(old != null){
-					renderer.line(old, c);
-				}
-				old = c;
-				
-			}
-		
-			renderer.line(old, getPos());
-		}
-		
-		
 		renderer.end();
+		
+		
 	}
 
 	@Override
