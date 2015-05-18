0fb2e5883608df7f409b96293eb3761560c5cf15
Andreas Brommund <brommund@kth.se>
2015-05-18 09:44:03 +0200

    Tweak: Format stuff and add comments, only in the actor package.

--- core/src/se/mad/copterplant/actor/Path.java
+++ core/src/se/mad/copterplant/actor/Path.java
@@ -8,85 +8,95 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.actions.RemoveAction;
 
 /**
  *
  * @author Andreas Brommund
  *
  */
-public class Path{
+public class Path {
 	private LinkedList<PathObject> path;
 
 	private Player player;
-	
-	//Remove the path stuff
-	private boolean destroyPath; 
+
+	// Remove the path stuff
+	private boolean destroyPath;
 	private int removeIndex;
-	//Timer stuff
+	// Timer stuff
 	private float deltaTime;
 	private float startTimer;
-	
-	public Path(Vector2 pos, Player player){
+
+	/**
+	 * Start a new path
+	 * 
+	 * @param the start pos of the path.
+	 * @param player
+	 */
+	public Path(Vector2 pos, Player player) {
 		path = new LinkedList<PathObject>();
 		path.add(new PathObject(pos));
-		
+
 		this.player = player;
-		
+
 		destroyPath = false;
 		removeIndex = 0;
-		deltaTime = .01f; //In seconds
+		deltaTime = .01f; // In seconds
 		startTimer = 0;
-		
+
 	}
 
 	/**
 	 * Add the node last to the the path.
+	 * 
 	 * @param node
 	 */
-	public void addNode(Vector2 node){
+	public void addNode(Vector2 node) {
 		path.addLast(new PathObject(node));
 	}
 
 	/**
 	 * Return the path object list.
+	 * 
 	 * @return LinkedList<PathObject>
 	 */
-	public LinkedList<PathObject> getPath(){
+	public LinkedList<PathObject> getPath() {
 		return path;
 	}
 
 	/**
 	 * Draw the path as circles.
+	 * 
 	 * @param renderer
 	 * @param playerPos
 	 * @param playerVel
 	 */
 	public void draw(ShapeRenderer renderer) {
-		for(PathObject p:path){
+		for (PathObject p : path) {
 			p.draw(renderer);
 		}
 	}
 
 	/**
-	 * Check collision between a rectangle and the path, if it is a collision the method is return true.
+	 * Check collision between a rectangle and the path, if it is a collision
+	 * the method is return true.
+	 * 
 	 * @param Rectangle
 	 * @return true or false
 	 */
-	public boolean isColliding(Rectangle rect){
-		if(path == null){
+	public boolean isColliding(Rectangle rect) {
+		if (path == null) {
 			player.die();
 			return false;
 		}
 		Iterator<PathObject> it = path.iterator();
 		int i = 0;
-		while(it.hasNext()){
-			if(rect.overlaps(it.next().getCollisionBox())){
+		while (it.hasNext()) {
+			if (rect.overlaps(it.next().getCollisionBox())) {
 				it.remove();
-				
-				if(!destroyPath){
-					destroyPath = true; 
-					removeIndex = i-1;
+
+				if (!destroyPath) {
+					destroyPath = true;
+					removeIndex = i - 1;
 				}
 				return true;
 			}
@@ -95,24 +105,24 @@ public class Path{
 		return false;
 	}
 
-	private void destroyPath(){
-		
-		if(removeIndex == path.size()){
+	private void destroyPath() {
+
+		if (removeIndex == path.size()) {
 			player.die();
 			return;
 		}
-		
-		if(removeIndex < 0){
+
+		if (removeIndex < 0) {
 			removeIndex = 0;
 		}
-		
-		if(path.size()>0){
-			if(removeIndex > 0){
+
+		if (path.size() > 0) {
+			if (removeIndex > 0) {
 				path.remove(removeIndex);
 				removeIndex--;
 			}
 			path.remove(removeIndex);
-		}else{
+		} else {
 			destroyPath = false;
 			removeIndex = 0;
 		}
@@ -120,34 +130,42 @@ public class Path{
 
 	/**
 	 * Update the Path
+	 * 
 	 * @param delta
 	 */
-	public void update(float delta){
-		if(startTimer <= 0){
-			if(destroyPath){
+	public void update(float delta) {
+		if (startTimer <= 0) {
+			if (destroyPath) {
 				destroyPath();
 			}
 			startTimer = deltaTime;
-		}else{
+		} else {
 			startTimer -= delta;
 		}
 	}
-	
-	public class PathObject{
+
+	public class PathObject {
 		private Rectangle collisionBox;
 		private Vector2 pos;
 		private float radius = 16;
 
+		/**
+		 * A new path object
+		 * 
+		 * @param the pos of the path object.
+		 */
 		public PathObject(Vector2 pos) {
-			collisionBox = new Rectangle(pos.x-radius,pos.y-radius,radius*2,radius*2);
+			collisionBox = new Rectangle(pos.x - radius, pos.y - radius,
+					radius * 2, radius * 2);
 			this.pos = pos;
 		}
 
 		/**
 		 * Draw the path object
+		 * 
 		 * @param renderer
 		 */
-		public void draw(ShapeRenderer renderer){
+		public void draw(ShapeRenderer renderer) {
 			renderer.begin(ShapeType.Filled);
 			renderer.setColor(Color.BLUE);
 			renderer.circle(pos.x, pos.y, radius);
@@ -155,7 +173,8 @@ public class Path{
 
 			renderer.begin(ShapeType.Line);
 			renderer.setColor(Color.YELLOW);
-			renderer.rect(collisionBox.x,collisionBox.y,collisionBox.width,collisionBox.height);
+			renderer.rect(collisionBox.x, collisionBox.y, collisionBox.width,
+					collisionBox.height);
 			renderer.end();
 		}
 
