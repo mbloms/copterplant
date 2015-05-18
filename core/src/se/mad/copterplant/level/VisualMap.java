67a693ceb0f9735c416afedc4ca7d0880ea8465c
David Skeppstedt <david.skeppstedt@gmail.com>
2015-05-18 08:41:55 +0200

    Tweak: Implemented transformation methods, both from screen to level space and vice versa.

--- core/src/se/mad/copterplant/level/VisualMap.java
+++ core/src/se/mad/copterplant/level/VisualMap.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * This is the visual representation of the leveldata stored in a backend 
@@ -97,9 +98,59 @@ public class VisualMap {
 			renderer.begin(ShapeType.Line);
 				renderer.rect(r.x,r.y,r.width,r.height);
 			renderer.end();
-		}
-		
-		
-		
+		}	
 	}
+	
+	
+	
+	
+	/**
+	 * Takes a Vector2 screen position and returns an int array with the position
+	 * relative the level. The position is returned in grid space, which means that 
+	 * resulting positions is integers.
+	 * As the position is relative it could also be outside the actual map/level. 
+	 * This is merely a coordination system transformation. 
+	 * The first element in the returned array is the x position and the last element is the y position.
+	 * @param position a screen position.
+	 * @return array of integers where the first element is the x-pos and the last is the y-pos.
+	 */
+	public static int[] ScreenToLevelCoordinates(Vector2 position) {
+		int[] levelPos = new int[2];
+		position.sub(X_OFFSET*TILE_SIZE, Y_OFFSET*TILE_SIZE);
+		int gridX = (int)Math.round(position.x/TILE_SIZE);
+		int gridY = (int)Math.round(position.y/TILE_SIZE);
+		levelPos[0] = gridX;
+		levelPos[1] = gridY;
+		return levelPos;
+	}
+	/**
+	 * Transforms a level coordinate to a screen coordinate.
+	 * This is relative to the bottom-left corner of the the screen.
+	 * @param x position in grid/level space.
+	 * @param y position in grid/level space
+	 * @return a vector with the same coordinates but in screen space.
+	 */
+	public static Vector2 LevelCoordinatesToScreen(int x, int y){
+		Vector2 screenPos = new Vector2();
+		float screenX = (X_OFFSET + x)*TILE_SIZE;
+		float screenY = (Y_OFFSET + y)*TILE_SIZE;
+		screenPos.x = screenX;
+		screenPos.y = screenY;
+		return screenPos;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
